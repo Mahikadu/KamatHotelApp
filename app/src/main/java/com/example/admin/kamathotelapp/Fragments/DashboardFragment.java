b4b74bdf0c26dba85676b4e091bfb9ba57182ac5
SudesiRushikesh <rushikesh@sudesi.in>
2017-06-22 18:01:35 +0530

    dashboard design

--- app/src/main/java/com/example/admin/kamathotelapp/Fragments/DashboardFragment.java
+++ app/src/main/java/com/example/admin/kamathotelapp/Fragments/DashboardFragment.java
@@ -5,10 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.admin.kamathotelapp.R;
 
@@ -16,6 +20,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DashboardFragment extends Fragment {
+
+    private AutoCompleteTextView autoLegal, autoProp, autoLoc, autoYr, autoQuarter, autoMonth;
+    String[] strLegalArray = null;
+    String[] strPropertyArray = null;
+    String[] strMonthArray = null;
+    String[] strYearArray = null;
+    String[] strQuarterArray = null;
+    String[] strLocArray = null;
+    String[] strLevel2Array = null;
+    String[] strLevel3Array = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +41,281 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
+        initView(view);
         return view;
+    }
+
+    void initView(View view) {
+        autoLegal = (AutoCompleteTextView)view.findViewById(R.id.autoLegalEntity);
+        autoProp = (AutoCompleteTextView)view.findViewById(R.id.autoProperty);
+        autoLoc = (AutoCompleteTextView)view.findViewById(R.id.autoLocation);
+        autoYr = (AutoCompleteTextView)view.findViewById(R.id.autoYear);
+        autoQuarter = (AutoCompleteTextView)view.findViewById(R.id.autoQuarter);
+        autoMonth = (AutoCompleteTextView)view.findViewById(R.id.autoMonth);
+
+        List<String> listLegal = Arrays.asList(getResources().getStringArray(R.array.legal_entity));
+        List<String> listProperty = Arrays.asList(getResources().getStringArray(R.array.property));
+        List<String> listMonth = Arrays.asList(getResources().getStringArray(R.array.month));
+        List<String> listYear = Arrays.asList(getResources().getStringArray(R.array.year));
+        List<String> listQuarter = Arrays.asList(getResources().getStringArray(R.array.quarter));
+        List<String> listLoc = Arrays.asList(getResources().getStringArray(R.array.location));
+
+        if (listLegal.size() > 0) {
+            strLegalArray = new String[listLegal.size()];
+            //   strLeadArray[0] = "Select Source Lead";
+            for (int i = 0; i < listLegal.size(); i++) {
+                strLegalArray[i] = listLegal.get(i);
+            }
+        }
+
+        if (listLegal != null && listLegal.size() > 0) {
+            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, strLegalArray) {
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View v = null;
+                    // If this is the initial dummy entry, make it hidden
+                    if (position == 0) {
+                        TextView tv = new TextView(getContext());
+                        tv.setHeight(0);
+                        tv.setVisibility(View.GONE);
+                        v = tv;
+                    } else {
+                        // Pass convertView as null to prevent reuse of special case views
+                        v = super.getDropDownView(position, null, parent);
+                    }
+                    // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                    parent.setVerticalScrollBarEnabled(false);
+                    return v;
+                }
+            };
+
+            adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            autoLegal.setAdapter(adapter1);
+        }
+
+        autoLegal.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoLegal.showDropDown();
+                return false;
+            }
+        });
+
+        if (listProperty.size() > 0) {
+            strPropertyArray = new String[listProperty.size()];
+            //   strLeadArray[0] = "Select Source Lead";
+            for (int i = 0; i < listProperty.size(); i++) {
+                strPropertyArray[i] = listProperty.get(i);
+            }
+        }
+
+        if (listProperty != null && listProperty.size() > 0) {
+
+            ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, strPropertyArray) {
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View v = null;
+                    // If this is the initial dummy entry, make it hidden
+                    if (position == 0) {
+                        TextView tv = new TextView(getContext());
+                        tv.setHeight(0);
+                        tv.setVisibility(View.GONE);
+                        v = tv;
+                    } else {
+                        // Pass convertView as null to prevent reuse of special case views
+                        v = super.getDropDownView(position, null, parent);
+                    }
+                    // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                    parent.setVerticalScrollBarEnabled(false);
+                    return v;
+                }
+            };
+
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            autoProp.setAdapter(adapter2);
+        }
+
+        autoProp.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoProp.showDropDown();
+                return false;
+            }
+        });
+
+        if (listLoc.size() > 0) {
+            strLocArray = new String[listLoc.size()];
+            //   strLeadArray[0] = "Select Source Lead";
+            for (int i = 0; i < listLoc.size(); i++) {
+                strLocArray[i] = listLoc.get(i);
+            }
+        }
+
+        if (listLoc != null && listLoc.size() > 0) {
+
+            ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, strLocArray) {
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View v = null;
+                    // If this is the initial dummy entry, make it hidden
+                    if (position == 0) {
+                        TextView tv = new TextView(getContext());
+                        tv.setHeight(0);
+                        tv.setVisibility(View.GONE);
+                        v = tv;
+                    } else {
+                        // Pass convertView as null to prevent reuse of special case views
+                        v = super.getDropDownView(position, null, parent);
+                    }
+                    // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                    parent.setVerticalScrollBarEnabled(false);
+                    return v;
+                }
+            };
+
+            adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            autoLoc.setAdapter(adapter3);
+        }
+
+        autoLoc.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoLoc.showDropDown();
+                return false;
+            }
+        });
+
+        if (listYear.size() > 0) {
+            strYearArray = new String[listYear.size()];
+            //   strLeadArray[0] = "Select Source Lead";
+            for (int i = 0; i < listYear.size(); i++) {
+                strYearArray[i] = listYear.get(i);
+            }
+        }
+
+        if (listYear != null && listYear.size() > 0) {
+
+            ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, strYearArray) {
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View v = null;
+                    // If this is the initial dummy entry, make it hidden
+                    if (position == 0) {
+                        TextView tv = new TextView(getContext());
+                        tv.setHeight(0);
+                        tv.setVisibility(View.GONE);
+                        v = tv;
+                    } else {
+                        // Pass convertView as null to prevent reuse of special case views
+                        v = super.getDropDownView(position, null, parent);
+                    }
+                    // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                    parent.setVerticalScrollBarEnabled(false);
+                    return v;
+                }
+            };
+
+            adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            autoYr.setAdapter(adapter4);
+        }
+
+        autoYr.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoYr.showDropDown();
+                return false;
+            }
+        });
+
+        if (listQuarter.size() > 0) {
+            strQuarterArray = new String[listQuarter.size()];
+            //   strLeadArray[0] = "Select Source Lead";
+            for (int i = 0; i < listQuarter.size(); i++) {
+                strQuarterArray[i] = listQuarter.get(i);
+            }
+        }
+
+        if (listQuarter != null && listQuarter.size() > 0) {
+
+            ArrayAdapter<String> adapter5 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, strQuarterArray) {
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View v = null;
+                    // If this is the initial dummy entry, make it hidden
+                    if (position == 0) {
+                        TextView tv = new TextView(getContext());
+                        tv.setHeight(0);
+                        tv.setVisibility(View.GONE);
+                        v = tv;
+                    } else {
+                        // Pass convertView as null to prevent reuse of special case views
+                        v = super.getDropDownView(position, null, parent);
+                    }
+                    // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                    parent.setVerticalScrollBarEnabled(false);
+                    return v;
+                }
+            };
+
+            adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            autoQuarter.setAdapter(adapter5);
+        }
+
+        autoQuarter.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoQuarter.showDropDown();
+                return false;
+            }
+        });
+
+        if (listMonth.size() > 0) {
+            strMonthArray = new String[listMonth.size()];
+            //   strLeadArray[0] = "Select Source Lead";
+            for (int i = 0; i < listMonth.size(); i++) {
+                strMonthArray[i] = listMonth.get(i);
+            }
+        }
+
+        if (listMonth != null && listMonth.size() > 0) {
+
+            ArrayAdapter<String> adapter6 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, strMonthArray) {
+                @Override
+                public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                    View v = null;
+                    // If this is the initial dummy entry, make it hidden
+                    if (position == 0) {
+                        TextView tv = new TextView(getContext());
+                        tv.setHeight(0);
+                        tv.setVisibility(View.GONE);
+                        v = tv;
+                    } else {
+                        // Pass convertView as null to prevent reuse of special case views
+                        v = super.getDropDownView(position, null, parent);
+                    }
+                    // Hide scroll bar because it appears sometimes unnecessarily, this does not prevent scrolling
+                    parent.setVerticalScrollBarEnabled(false);
+                    return v;
+                }
+            };
+
+            adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            autoMonth.setAdapter(adapter6);
+        }
+
+
+        autoMonth.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                autoMonth.showDropDown();
+                return false;
+            }
+        });
     }
 }
